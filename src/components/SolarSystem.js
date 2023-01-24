9d57d3196b1ed03e52785d149b1391d0abd8cc79
Sergio <sergioajk@hotmail.com>
2023-01-24 15:10:51 -0300

    requisito 02

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system"> Div do Solar System </div>
+    );
+  }
+}
+export default SolarSystem;
